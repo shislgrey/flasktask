1237f7a4148de88309e2fd294a0e19de2448daf6
Grey <shisler.grace@gmail.com>
2024-03-18 14:12:59 -0700

    removed placeholder user template

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,8 @@ from app import db
 from app.models import User
 from flask_login import logout_user
 from flask_login import login_required
+from flask import request
+from urllib.parse import urlsplit
 
 @app.route('/')
 @app.route('/index')
@@ -24,7 +26,7 @@ def index():
             'body': 'I ship Polly and Maladict'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -36,7 +38,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('/index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
