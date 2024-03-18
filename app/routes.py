b1ca60a0955c0dcec6695ea5c6c1d134d3e1106f
Grey <shisler.grace@gmail.com>
2024-03-18 13:13:13 -0700

    added logout

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,11 @@ from flask import render_template
 from app import app
 from app.forms import LoginForm
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
+from flask_login import logout_user
 
 @app.route('/')
 @app.route('/index')
@@ -20,8 +25,19 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        user = db.session.scalar(sa.select(User).where(user.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
