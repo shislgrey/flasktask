b8df009b6b4d3be14558f3b2fea220c655f5e378
Grey <shisler.grace@gmail.com>
2024-03-18 12:56:54 -0700

    imported usermixin

--- app/models.py
+++ app/models.py
@@ -4,8 +4,9 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
