51d7800e860bebcc1a8982c74bae958d7a4c5a1a
Grey <shisler.grace@gmail.com>
2024-03-19 14:49:35 -0700

    added user and test login credentials

--- app/routes.py
+++ app/routes.py
@@ -26,21 +26,22 @@ def index():
             'body': 'I ship Polly and Maladict'
         }
     ]
-    return render_template('index.html', title='Home', posts=posts)
+    return render_template('index.html', title='Home', user=user, posts=posts)
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = db.session.scalar(sa.select(User).where(user.username == form.username.data))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or urlsplit(next_page).netloc != '':
-            next_page = url_for('/index')
+            next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
