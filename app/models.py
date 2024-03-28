eb72200806c132349d0b84def4b5d343ff484d76
Grey <shisler.grace@gmail.com>
2024-03-28 14:41:58 -0700

    more password reset logic

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from app import login, db
+from app import login, db, app
 from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
@@ -6,6 +6,8 @@ import sqlalchemy.orm as so
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
+from time import time
+import jwt
 
 followers = sa.Table(
     'followers',
@@ -73,6 +75,19 @@ class User(UserMixin, db.Model):
             .group_by(Post)
             .order_by(Post.timestamp.desc())
         )
+    
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return db.session.get(User, id)
     
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
