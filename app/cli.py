7b0e5e5f6012c907b07388c368a46b0ac15c160e
Grey <shisler.grace@gmail.com>
2024-04-02 09:17:16 -0700

    minor refactoring

--- app/cli.py
+++ app/cli.py
@@ -2,18 +2,6 @@ import os
 from app import app
 import click
 
-
-@translate.command()
-@click.argument('lang')
-def init(lang):
-    """Initialize a new language."""
-    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
-        raise RuntimeError('extract command failed')
-    if os.system(
-            'pybabel init -i messages.pot -d app/translations -l ' + lang):
-        raise RuntimeError('init command failed')
-    os.remove('messages.pot')
-
 @app.cli.group()
 def translate():
     """Translation and localization commands."""
@@ -33,3 +21,14 @@ def compile():
     """Compile all languages."""
     if os.system('pybabel compile -d app/translations'):
         raise RuntimeError('compile command failed')
+
+@translate.command()
+@click.argument('lang')
+def init(lang):
+    """Initialize a new language."""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system(
+            'pybabel init -i messages.pot -d app/translations -l ' + lang):
+        raise RuntimeError('init command failed')
+    os.remove('messages.pot')
